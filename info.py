ae11a7b383a489a9bc5615201bc8a5a1508032cf
bothub33213 <113312962+bothub33213@users.noreply.github.com>
2023-01-26 08:01:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -47,7 +47,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 SHORTENR_URL = environ.get('SHORTENR_URL', 'Clicksfly.com')
-SHORTNER_API = environ.get('SHORTNER_API', '')
+SHORTNER_API = environ.get('SHORTNER_API', '7280ef965e4116cd6ad1d198da68006b35c1db23')
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+GOFte-Rz2tcxODg1')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+mCMdCb_ymAowZmNl')
 MSG_ALRT = environ.get('MSG_ALRT', 'Wʜᴀᴛ Aʀᴇ Yᴏᴜ Lᴏᴏᴋɪɴɢ Aᴛ ?')
